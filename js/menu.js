e7b5643b6c3ac707a842d0cf05a78091307daf04
王凌军 <wanglingjun@xdaocloud.com>
2017-12-22 15:22:38 +0800

    修改页面流和预览

--- js/menu.js
+++ js/menu.js
@@ -1364,7 +1364,7 @@ $(function(){
 	        success:function(data){
 	            if(data.code === 0){
 	                if(data.data.htmlJson){
-	                    displayPage(data.data);
+                        DataIndexes.generate(data.data,$(".mn-htmlmain"));
 	                }
 	            }
 	        },
@@ -1374,36 +1374,3 @@ $(function(){
 	    });
 	}
 
-	
-    // 显示BI页面数据
-    function displayPage(data){
-        // 遍历数据,生成图形
-        var html = '';
-        $.each(data.htmlJson.controls,function(index,val){
-            var text = ''            // html
-                ,style =  val.style  // 宽、高
-                ,dataType = val.customData.dataType;  // 类型
-
-            // 判断图形、表格、文本、图片、按钮
-            // 如果是文本和图片，则复制内容不同
-            if(dataType === "text" || dataType === "button" || dataType === "image"){
-                if(val.customData.controls){
-                    text = val.customData.controls.html;
-                }else if(dataType === "text"){
-                    text = '<div class="content-text edit"><div contenteditable="false" spellcheck="true" data-medium-editor-element="true" role="textbox" aria-multiline="true" data-placeholder="请输入文本" data-medium-focused = "true"></div></div>';
-                }else if(dataType === "button"){
-                    text = '<div class="content-button"><button></button></div>';
-                }
-            }else if(dataType === "table" || dataType === "chart"){
-                // 将数据存入检索数据中
-                var chart_date = {
-                    'cid':val.cid,
-                    "type":val.type,
-                    "queryJson":val.queryJson,
-                };
-                DataIndexes.inAjax(chart_date,val.cid);
-            }
-            html += '<div linkPageId="'+val.linkPageId+'" id="'+ val.cid +'" type="'+ val.type +'" data-type="'+ val.customData.dataType +'" style="height:'+ style.height +'px;width:'+ style.width +'px;top:'+ style.top +'px;left:'+ style.left +'px;z-index:'+ val.displayLevel +'" class="resize-item">'+ text +'</div>';
-        });
-        $(".mn-htmlmain").empty().append(html);
-    }
