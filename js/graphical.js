04846b39878140ace883fc44ae469ca47672ec78
王凌军 <wanglingjun@xdaocloud.com>
2017-12-25 16:27:34 +0800

    修改bug

--- js/graphical.js
+++ js/graphical.js
@@ -5,14 +5,14 @@ var DataIndexes = {
 
     // 为测试环境
     generate:function(data,storage){
+        storage.empty();
         let html = '';
         $.each(data.htmlJson.controls,function(index,val){
             let text = DataIndexes.text(val.customData.controls,val.customData.dataType);
             const style =  val.style;
-
             html = '<div linkPageId = "'+ val.linkPageId +'"  id="'+ val.cid +'" type="'+ val.type +'" data-type="'+ val.customData.dataType +'" style="height:'+ style.height +'px;width:'+ style.width +'px;top:'+ style.top +'px;left:'+ style.left +'px;z-index:'+ val.displayLevel +'" class="resize-item">'+ text +'</div>';
 
-            storage.empty().append(html);
+            storage.append(html);
 
             if(val.customData.dataType === "chart"){
                 // 将数据存入检索数据中
